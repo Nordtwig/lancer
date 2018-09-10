b9879fe10c0112c57775902ee4631dc443e4169d
Nordtwig <nordqvist.gaming@gmai.com>
2018-09-10 21:14:57 +0200

    Added movement. Added Lance model. Added Player 2 (But slaved to same controls. For now).

--- Lancer/Assets/Player.cs
+++ Lancer/Assets/Player.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //Set speed of rotation and movement
+    [SerializeField] int movementSpeed; //A value around 3-10 is suitable
+    [SerializeField] int turnSpeed; //A value around 200-500 is suitable
+    [SerializeField] float angularSpeed; //A value between 0.6-0.8 is suitable
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
 }
